e0d4e797cf73ef579b9b2f20dc9d1bdf79f0bdb2
KassieSanisclaws <Kassie35dev@gmail.com>
2024-06-24 14:46:00 -0400

    Initial Commit

---
+++ PhotoPicker_Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoPicker_Example
+//
+//  Created by Kadeem Cherman on 2024-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoPicker_Example/PhotoPicker_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPicker_ExampleApp.swift
+//  PhotoPicker_Example
+//
+//  Created by Kadeem Cherman on 2024-06-24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPicker_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
